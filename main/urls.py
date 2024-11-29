86f1476ae962b515594dcbb5a6fb488a5789ee0e
Aidar Putilov <putiloff@gmail.com>
2024-11-29 14:18:46 +0500

    Add permissions.py

--- main/urls.py
+++ main/urls.py
@@ -12,17 +12,4 @@ router.register(r"", HabitViewSet, basename="habits")
 
 urlpatterns = [
     path("public/", HabitPublicListAPIView.as_view(), name="public_habits"),
-    # path("lesson/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson_retrieve"),
-    # path("lesson/create/", LessonCreateAPIView.as_view(), name="lesson_create"),
-    # path(
-    #     "lesson/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lesson_delete"
-    # ),
-    # path(
-    #     "lesson/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lesson_update"
-    # ),
-    # path(
-    #     "subscription/",
-    #     SubscriptionAPIView.as_view(),
-    #     name="subscription",
-    # ),
 ] + router.urls
