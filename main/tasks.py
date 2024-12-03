de5eb5a4534185157d16329e0bbbee9a7da4267b
Aidar Putilov <putiloff@gmail.com>
2024-12-03 05:06:07 +0500

    Add comments

--- main/tasks.py
+++ main/tasks.py
@@ -16,8 +16,11 @@ def send_telegram_reminder():
 
     for habit in habits:
         tg_chat_id = habit.owner.tg_chat_id
+
+        # ID Телеграм указан и привычка полезная
         if tg_chat_id and not habit.is_enjoy:
 
+            # Флаг на условие времени отправки
             flag = False
             if not habit.send_time:
                 flag = True
@@ -28,12 +31,20 @@ def send_telegram_reminder():
                 message = (f"Задание на сегодня: {habit.name},"
                            f" место: {habit.place},"
                            f" время: {habit.start_time}")
+
+                # Указано вознаграждение
                 if habit.reward:
                     message += f"\nЗа выполнение: {habit.reward}"
+
+                # Указана связанная привычка
                 if habit.related_habit:
                     message += (f"\nЗа выполнение: {habit.related_habit},"
                                 f" место: {habit.related_habit.place},"
                                 f" время: {habit.related_habit.start_time}")
+
+                # Отправка сообщения
                 send_telegram_message(tg_chat_id, message)
+
+                # Сдвиг времени отправки на величину периода
                 habit.send_time = current_time + timedelta(days=habit.period)
                 habit.save()
